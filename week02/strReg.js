cb6994a1fd9f294890ce41f521c87c8e40e13b06
sunshine <fejavu@foxmail.com>
2020-04-22 00:08:09 +0800

    week02 home work regNum&regStr$UTF-8 Encoding

--- week02/strReg.js
+++ week02/strReg.js
@@ -8,3 +8,6 @@
  *      \Exc
  *  Single String Characters
  */
+
+let regDobleString = /"(?:["\n\\\r\u2028\u2029]|\\(?:["'\\bfnrtv\n\r\u2028\u2029]|\r\n)|\\x[0-9a-fA-F]{2}|\\u[0-9a-fA-F]{4}|\\[^0-9ux"'\\bfnrtv\n\r\u2028\u2029])*"/g;
+let regSigleString = /'(?:["\n\\\r\u2028\u2029]|\\(?:["'\\bfnrtv\n\r\u2028\u2029]|\r\n)|\\x[0-9a-fA-F]{2}|\\u[0-9a-fA-F]{4}|\\[^0-9ux"'\\bfnrtv\n\r\u2028\u2029])*'/g;
--- week02/uft8_encoding.js
+++ week02/uft8_encoding.js
@@ -1,13 +1,56 @@
 /**
- * UTF16Encoding(cp)
- * The UTF16Encoding of a numeric code point value, cp, is determined as follows:
-1. Assert: 0 ≤ cp ≤ 0x10FFFF.
-2. If cp ≤ 0xFFFF, return cp.
-3. Let cu1 be floor((cp - 0x10000) / 0x400) + 0xD800.
-4. Let cu2 be ((cp - 0x10000) modulo 0x400) + 0xDC00.
-5. Return the code unit sequence consisting of cu1 followed by cu2.
+ * 
+1	7	U+0000	U+007F	0xxxxxxx			
+2	11	U+0080	U+07FF	110xxxxx	10xxxxxx		
+3	16	U+0800	U+FFFF	1110xxxx	10xxxxxx	10xxxxxx	
+4	21	U+10000	U+10FFFF[12]	11110xxx	10xxxxxx	10xxxxxx	10xxxxxx
  */
 
-function utf8_encoding() {
+function UTF8_Encoding(str) {
+  let res = '';
+  if(str.length<1) return res;
   
+  for(let i=0;i<str.length; i++) {
+    res += toStrHex(str.charCodeAt(i));
+  }
+  return res; 
 }
+
+function toStrHex(num) {
+  if(num < 0x007F) {
+    return '\\x'+num.toString(16);    
+  }else if(num <0x7FF){
+    let temp = num.toString(2);
+    temp = '110'+temp.slice(0,5)+'10'+temp.slice(5,11);
+    return binaryStrToHex(temp);
+  }else if(num <0xFFFF){
+    let temp = num.toString(2);
+    temp = '1110'+temp.slice(0,4)+'10'+temp.slice(4, 10)+'10'+temp.slice(10, 16);
+    return binaryStrToHex(temp);
+  }else if(num < 0x10FFFF){
+    let temp = num.toString(2);
+    temp = '11110'+temp.slice(0,3)+'10'+temp.slice(3, 9)+'10'+temp.slice(9, 15)+'10'+temp.slice(15, 21);
+    return binaryStrToHex(temp);
+  }
+}
+
+function binaryStrToHex(binaryStr){
+  let res = '';
+  let byteCount = binaryStr.length / 4;
+
+  for(let i=0;i<byteCount;i++) {
+    let tempStr = binaryStr.slice(i, i+4);
+    res += '\\x'+byteToNum(tempStr).toString(16);
+  }
+}
+
+function byteToNum(str) {
+  let res = 0;
+   for(let i=0;i<str.length;i++) {
+     res += str[i]*Math.pow(16, 3-i);
+   }
+   return res;
+}
+
+let demoStr = 'winter is coming';
+// console.log(UTF8_Encoding(demoStr));
