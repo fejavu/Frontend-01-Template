38147857288d0b0fe2c933adbe403221f5aafe7a
sunshine <fejavu@foxmail.com>
2020-04-21 08:21:21 +0800

    week02 String Literals and UTF8Encoding

---
+++ week02/strReg.js
@@ -0,0 +1,10 @@
+/**
+ * String Literal
+ *  Double String Characters
+ *    DoubleStringChar
+ *      SourceChar(non " or \ or LineTerminator)
+ *      <LS>
+ *      <PS>
+ *      \Exc
+ *  Single String Characters
+ */
--- week02/uft8_encoding.js
+++ week02/uft8_encoding.js
@@ -1,3 +1,13 @@
+/**
+ * UTF16Encoding(cp)
+ * The UTF16Encoding of a numeric code point value, cp, is determined as follows:
+1. Assert: 0 ≤ cp ≤ 0x10FFFF.
+2. If cp ≤ 0xFFFF, return cp.
+3. Let cu1 be floor((cp - 0x10000) / 0x400) + 0xD800.
+4. Let cu2 be ((cp - 0x10000) modulo 0x400) + 0xDC00.
+5. Return the code unit sequence consisting of cu1 followed by cu2.
+ */
+
 function utf8_encoding() {
   
 }
