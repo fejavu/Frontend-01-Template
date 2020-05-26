0afb4a31f6a44133802678659bc136009a2ad531
sunshine <fejavu@foxmail.com>
2020-05-26 23:55:35 +0800

    week07 homework 0526

---
+++ week07/toyBrowser/server.js
@@ -0,0 +1,36 @@
+/**
+ * Create a server with node.js
+ */
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log("request received");
+  // console.log(req.headers);
+  res.setHeader('Content-Type', 'text/html');
+  res.setHeader('x-foo', 'bar');
+  res.writeHead(200, { 'Content-Type': 'text/plain'});
+  // res.end("this is the response");
+  res.end(
+`<html class="a">
+<head>
+    <style>
+body div #myid{
+    width:100px;
+    background-color: #ff5000;
+}
+body div img{
+    width:30px;
+    background-color: #ff1111;
+}
+    </style>
+</head>
+<body>
+    <div>
+        <img id="myid"/>
+        <img />
+    </div>
+</body>
+</html>`);
+});
+
+server.listen(8088); 
