1c1eac73abb6bd7269ec0908a434ffaa28f40814
sunshine <fejavu@foxmail.com>
2020-06-17 23:46:36 +0800

    Homework Week10 init

--- week07/toyBrowser/server.js
+++ week07/toyBrowser/server.js
@@ -5,11 +5,9 @@ const http = require("http");
 
 const server = http.createServer((req, res) => {
   console.log("request received");
-  // console.log(req.headers);
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('x-foo', 'bar');
   res.writeHead(200, { 'Content-Type': 'text/html'});
-  // res.end("this is the response");
   res.end(
 `<html class="a">
 <head>
--- week09/NOTE.md
+++ week09/NOTE.md
@@ -1,1 +1,19 @@
-# 每周总结可以写在这里
+# 每周总结WEEK09
+
+### CSS 动画与绘制
+
+Animation
+- @keyframe 关键帧
+- name / duration / timing-function(ease) / delay / ...
+
+Transition
+
+### CSS 渲染与颜色
+CMYK 和 RGB
+
+HSL 和 HSV
+
+形状 SVGls 
+
+作业 CSS 属性归类
+layout、render/draw、interactive、other、svg
