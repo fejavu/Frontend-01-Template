00ab4516baa2fe54bf8abb7b27bb9cd2e2b9e843
sunshine <fejavu@foxmail.com>
2020-07-08 23:25:28 +0800

    homework Week13 initial

---
+++ week13/component.js
@@ -0,0 +1,21 @@
+class myComponent {
+  constructor(config) {
+    this.state = {
+      i: 1,
+    }
+  }
+
+  get prop1() {}
+
+  set prop1() {}
+
+  setAttribute() {}
+
+  getAttribute(attr, value) {}
+
+  get children() {}
+  set children() {}
+}
+
+
+class tabaoCarousel {}
